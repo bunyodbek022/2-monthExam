d5acd760de939e3d996b442704fa4e73c2abbb9d
Bunyodbek G'ulomjonov <bunyodbek@172-15-29-111.lightspeed.mmphtn.sbcglobal.net>
2025-10-14 11:16:12 +0500

    Bazi kamchiliklar toldirildi

--- src/validation/columns.validation.js
+++ src/validation/columns.validation.js
@@ -1,12 +1,12 @@
 import Joi from "joi";
 
 export const columnsCreateSchema = Joi.object({
-    name: Joi.string().required(),
-    board_id: Joi.string().required
+    name: Joi.string().min(3).max(30).pattern(/^[^\/$#0-9]+$/).required(),
+    board_id: Joi.string().required()
     
     });
 
 export const columnsUpdateSchema = Joi.object({
-    name : Joi.string(),
+    name : Joi.string().min(3).max(30).pattern(/^[^\/$#0-9]+$/),
     board_id: Joi.string()
   });
